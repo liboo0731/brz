9fba6d24316b10307476e9198df5269a9af58fc5
liboo0731 <striveliboo@163.com>
2020-09-13 11:14:29 +0800

    新增csv转json程序

---
+++ csv2json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import json
+import csv
+
+if __name__ == '__main__':
+    print('-- starting --')
+    c_title = ('id', 'name', 'num', 'descr', 'link')
+    with open('data.csv', 'r') as cf:
+        reader = csv.DictReader(cf, c_title)
+        next(reader)
+        with open('data.json', 'w') as jf:
+            data_dict = {
+                "title": "既然选择了远方，便只顾风雨兼程！",
+                "subtitle": "公众号：博人撰",
+                "xstitle": "高效、便捷！",
+                "data": [row for row in reader]
+            }
+            jf.write(json.dumps(data_dict))
+    print('-- success --')
